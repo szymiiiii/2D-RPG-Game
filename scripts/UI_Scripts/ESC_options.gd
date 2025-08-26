e7c5b8074e1bd43cc6f9d78e179fdbaf36930158
szymi <szymi@debian.szymi>
2025-08-26 16:25:14 +0200

    drobne poprawki

---
+++ scripts/UI_Scripts/ESC_options.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+
+func _on_resume_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_return_pressed() -> void:
+	pass # Replace with function body.
