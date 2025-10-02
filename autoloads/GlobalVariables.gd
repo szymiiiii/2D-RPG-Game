c86f9abf6723e604529950bbc3f9580fac78dd56
jd92910 <jdrew3081@gmail.com>
2025-10-02 16:23:47 +0200

    Zmiana

--- autoloads/GlobalVariables.gd
+++ autoloads/GlobalVariables.gd
@@ -1,1 +1,10 @@
 extends Node
+
+@export var health: int = 100
+@export var curr_health: int = 0
+@export var first: bool = false
+@export var penguin: String = ""
+
+func _set_health() -> void:
+	#if first == false:
+		pass
