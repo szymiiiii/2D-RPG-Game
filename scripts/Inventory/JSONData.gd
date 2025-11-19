5163b5535d2e2bb51c6c3cd5304123162716a82f
jd92910 <jdrew3081@gmail.com>
2025-11-19 20:17:26 +0100

    Inventory(bad)

--- scripts/Inventory/JSONData.gd
+++ scripts/Inventory/JSONData.gd
@@ -5,9 +5,7 @@ var item_data: Dictionary
 func _ready() -> void:
 	item_data = LoadData("res://Data/ItemData.json")
 
-func LoadData(file_path):
-	var json_data = FileAccess.open(file_path, FileAccess.READ)
-	var file_data = JSON.new()
-	json_data.parse(file_data.get_as_text())
-	file_data.close()
-	return json_data.get_data()
+func LoadData(file_path : String):
+	var file_data = FileAccess.get_file_as_string(file_path)
+	var json_data = JSON.parse_string(file_data)
+	return json_data
