0bcc8d60674f5c9e1e55362437a202fcf43a0d64
szymiiiii <106547250+szymiiiii@users.noreply.github.com>
2025-11-27 15:30:02 +0100

    basic enemy 2
    working basic enemy mostly without errors and buggs

--- scripts/Enemies/basic_enemy.gd
+++ scripts/Enemies/basic_enemy.gd
@@ -1,3 +1,15 @@
 extends Node2D
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
+@onready var area_2d: Area2D = $Area2D
+
+var is_in_battle = false
+
+func _ready() -> void:
+	SignalBus.battle_ended.connect(_battle_ended)
+	
+func _battle_ended():
+	if is_in_battle:
+		area_2d.process_mode = Node.PROCESS_MODE_DISABLED
+		self.visible = false
+		is_in_battle = false
